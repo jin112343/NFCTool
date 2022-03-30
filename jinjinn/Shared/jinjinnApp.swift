a09d6aa6e30127b927d22773f6d28c3f38aa1a31
ミゾイジン <42370001+jin112343@users.noreply.github.com>
2022-03-30 14:43:13 +0900

    Add files via upload

---
+++ jinjinn/Shared/jinjinnApp.swift
@@ -0,0 +1,17 @@
+//
+//  jinjinnApp.swift
+//  Shared
+//
+//  Created by 溝井迅 on 2022/03/17.
+//
+
+import SwiftUI
+
+@main
+struct jinjinnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
